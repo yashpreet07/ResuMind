43d445debc86bc587ab0247202ac0046b9eff725
LogicMage <logicmage07@gmail.com>
2025-08-30 01:51:56 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,47 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {Navbar} from "~/components/navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    const {auth} = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated)    navigate('/auth?next=/');
+    },[auth.isAuthenticated])
+
+  return (
+      <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+          <Navbar />
+          <section className="main-section">
+                  <div className="page-heading py-16">
+                      <h1>Track Your Application & Resume Rating</h1>
+                      <h2>Review Your Submission and check AI-powered feedback</h2>
+                  </div>
+
+
+              {resumes.length > 0 && (
+              <div className="resumes-section">
+                  {resumes.map((resume) => (
+                      <ResumeCard key = {resume.id} resume = {resume} />
+                  ))}
+              </div>
+              )}
+       </section>
+
+
+
+      </main>)
 }
